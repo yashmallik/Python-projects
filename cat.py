38cbbaf43dc70a6747f8342f342773a09ce6383f
yashmallik <76205209+yashmallik@users.noreply.github.com>
2021-05-17 22:10:35 +0530

    Add files via upload

---
+++ cat.py
@@ -0,0 +1,12 @@
+fhand = open(input("Enter file name: "))
+day = dict()
+for line in fhand:
+    words = line.split()
+    if len(words) < 3 or words[0] != 'From':
+        continue
+    else:
+        if words[2] not in day:
+            day[words[2]] = 1
+        else:
+            day[words[2]] +=1
+print(day)
