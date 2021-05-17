a6f3a93fb51fbf6dbcf6081b75b8b2e6c97ec9c3
yashmallik <76205209+yashmallik@users.noreply.github.com>
2021-05-17 22:09:37 +0530

    Add files via upload

--- sender.py
+++ sender.py
@@ -1,10 +1,14 @@
-fhand = open('mbox.txt')
+mhand = input('ENTER FILE NAME : ')
+fhand = open(mhand)
 names = list()
+count = 0
 for lines in fhand:
-    lines.rstrips()
-    if lines.startwith('FROM'):
-        key = lines[2]
-        names.append(key)
-    else:
-        continue
+    word = lines.split()
+    if words[0] !== ('FROM'): continue
+    count = count + 1
+    print(word[2])
+
+
+    names.append(words[2])
 print(names)
+print("There are"),count,("people send message")
