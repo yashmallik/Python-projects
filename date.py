3d2abb05843aa81ba4fd38e465faad9f75730e4d
yashmallik <76205209+yashmallik@users.noreply.github.com>
2021-05-28 21:22:17 +0530

    Add files via upload

---
+++ date.py
@@ -0,0 +1,4 @@
+# Hello, the correct definition for leap year is: "The year has to be divisible by 4 and not exactly divisible by 100, unless it is divisible by 400".
+# 1600 is a leap year, but 1700, 1800 and 1900 aren't leap years
+import calendar
+print(calendar.calendar(2020))
