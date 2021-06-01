d70243908dcaff020638517aa7a968ecf67347d1
yashmallik <76205209+yashmallik@users.noreply.github.com>
2021-06-01 23:33:29 +0530

    Update determine_trangle.py

--- determine_trangle.py
+++ determine_trangle.py
@@ -13,5 +13,5 @@ while True :
             print ("This trangle does not exists")
             break
     except :
-        print ("Please enter numeric value")
+        print ("Please enter correct numeric value")
         continue
