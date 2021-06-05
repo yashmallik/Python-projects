d865adda2ce06555d387d9efb6ee90ab74194569
yashmallik <76205209+yashmallik@users.noreply.github.com>
2021-06-05 22:54:16 +0530

    Update roots_quadratic_equation.py

--- roots_quadratic_equation.py
+++ roots_quadratic_equation.py
@@ -13,5 +13,7 @@ try :
         x1 = (-y + sqrt(a))/2*x
         x2 = (-y - sqrt(a))/2*x
         print("x1 = %.2f; x2 = %.2f" %(x2,x2))
-else :
-    print("there is no real root")
+    else :
+        print("there is no real root")
+except :
+    print("Enter numeric value")
