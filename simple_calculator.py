2310d52a7d07feb301c679f90b2c13897015ad1d
yashmallik <76205209+yashmallik@users.noreply.github.com>
2021-06-25 22:49:49 +0530

    Update simple_calculator.py

--- simple_calculator.py
+++ simple_calculator.py
@@ -3,23 +3,25 @@ print("loading program---------")
 print("Zero operation terminate the program!")
 print('________________________________________________________')
 while True :
-    
-    opert = input("Choose operator(+, -, *, /): ")
-    if opert == 0:
-        break
-    if opert in ('+','-','*','/'):
-        x = float(input("Enter your first number: "))
-        y = float(input(str(x)+" "+str(opert) +" (Enter your last number): "))
-        if opert == "+":
-            print("%.2f" % (x+y))
-        elif opert == "-" :
-            print("%.2f" % (x-y))
-        elif opert == "*":
-            print ("%.2f" % (x*y))
-        elif opert == "/":
-            if y != 0:
-                print("%.2f" % (x/y))
+    try:
+        opert = input("Choose operator(+, -, *, /): ")
+        if opert == 0:
+            break
+        if opert in ('+','-','*','/'):
+            x = float(input("Enter your first number: "))
+            y = float(input(str(x)+" "+str(opert) +" (Enter your last number): "))
+            if opert == "+":
+                print("%.2f" % (x+y))
+            elif opert == "-" :
+                print("%.2f" % (x-y))
+            elif opert == "*":
+                print ("%.2f" % (x*y))
+            elif opert == "/":
+                if y != 0:
+                    print("%.2f" % (x/y))
+                else:
+                    print("Error! Cannot divided by zero")
             else:
-                print("Error! Cannot divided by zero")
-        else:
-            print("invalid operator!")
+                print("Invalid operator!")
+    except:
+        print("Invalid Input")
