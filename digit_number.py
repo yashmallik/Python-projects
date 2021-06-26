c906732ed2acd2e389828e4653a7627f5053b7c7
yashmallik <76205209+yashmallik@users.noreply.github.com>
2021-06-26 20:41:14 +0530

    Update digit_number.py

--- digit_number.py
+++ digit_number.py
@@ -1,8 +1,11 @@
-x = int(input("Insert your numeric value"))
-x = abs(x)
-count = 1
-x //= 10
-while x > 0:
-    x // = 10
-    count +=1
-print("The number of integer is: ", count)
+try:
+    x = int(input("Insert your numeric value"))
+    x = abs(x)
+    count = 1
+    x //= 10
+    while x > 0:
+        x // = 10
+        count +=1
+    print("The number of integer is: ", count)
+except:
+    print("Invalid input")
