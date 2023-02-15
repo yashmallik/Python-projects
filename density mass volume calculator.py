b214020145090c22bd635950804d7c92a2a174e9
Yash mallik <yashmallik12@gmail.com>
2023-02-16 00:00:03 +0530

    init

--- density mass volume calculator.py
+++ density mass volume calculator.py
@@ -1,18 +1,20 @@
-try :
+try:
     cal = input("Please choose the unit (m, v, d): ")
 
-    if cal == "m" :
+    result = 0
+
+    if cal == "m":
         v = float(input("volumn: "))
         d = float(input("Density: "))
         result = d*v
-    elif cal == "v" :
+    elif cal == "v":
         m = float(input("Mass: "))
         d = float(input("Density: "))
         result = m/d
-    elif cal == "d"  :
+    elif cal == "d":
         m = float(input("Mass: "))
         v = float(input("Volumn: "))
         result = m/v
     print("%.2f" % result)
-except : 
-    print ("Please enter correct value")
+except:
+    print("Please enter correct value")
