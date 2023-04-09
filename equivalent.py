ab94972f3bea6af251642091303bfe6201145d5a
Yash mallik <yashmallik12@gmail.com>
2023-04-09 21:01:02 +0530

    innit

---
+++ equivalent.py
@@ -0,0 +1,6 @@
+def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+    str1 = "".join(word1)
+    str2 = "".join(word2)
+    if str1 == str2:
+        return True
+    return False
